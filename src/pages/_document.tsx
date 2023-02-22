e67b343b8d2799096a669fd314d52b02c5265af8
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-02-22 21:12:50 +0700

    rfc: add fonts link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -13,7 +13,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Epilogue:wght@500;600;700&family=Inter:wght@300;400;500&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </Head>
       <body>
         <Main />
