8a33bcad941f45c8df37d32b17050554f8378b1f
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-02-22 21:42:19 +0700

    feat: implement custom login page

--- src/components/auth/login/LoginScreen.tsx
+++ src/components/auth/login/LoginScreen.tsx
@@ -1,16 +1,40 @@
-import { type NextPage } from "next"
-import { type getProviders } from "next-auth/react"
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { type NextPage } from "next";
+import { signIn, type getProviders } from "next-auth/react";
 
 interface ILoginScreen {
-  providers: Awaited<ReturnType<typeof getProviders>>
+  providers: Awaited<ReturnType<typeof getProviders>>;
 }
 
 const LoginScreen: NextPage<ILoginScreen> = ({ providers }) => {
   return (
-    <div>
-      LoginScreen
-    </div>
-  )
-}
+    <div className="flex min-h-screen items-center justify-center bg-[#DFECF1] font-epilogue">
+      <div className="select-none rounded-xl bg-black p-6 text-white">
+        <div className="space-y-2 py-4 text-white">
+          <h1 className="text-4xl font-semibold">Login</h1>
+          <p className="text-sm font-light text-stone-300">
+            Please login before accessing dashboard!
+          </p>
+        </div>
 
-export default LoginScreen
+        <div className="mt-4">
+          <div className="flex flex-col space-y-2">
+            {Object.values(providers!).map(({ id, name }) => (
+              <div key={name}>
+                <button
+                  className="w-full rounded border-2 border-transparent bg-white py-2.5 text-lg font-semibold text-black duration-300 hover:border-white hover:bg-black hover:text-white"
+                  onClick={() => signIn(id)}
+                >
+                  Login with {name}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
