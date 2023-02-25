ae4b633d6c4372c47c9582d258350ea522bbfd2b
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-02-25 15:41:26 +0700

    feat: implement navbar base

---
+++ src/components/common/navbar/base/NavbarBase.tsx
@@ -0,0 +1,19 @@
+import { type ReactNode, type FC } from "react";
+
+interface INavbarBase {
+  children: ReactNode;
+}
+
+const NavbarBase: FC<INavbarBase> = ({ children }) => {
+  return (
+    <div className="absolute min-h-screen p-[1.5vh]">
+      <div className="flex min-h-[97vh] flex-col justify-between rounded-xl bg-black py-10">
+        <h1 className="px-4 font-semibold text-white">LMS</h1>
+
+        <div className="flex flex-col items-center space-y-20">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarBase;
