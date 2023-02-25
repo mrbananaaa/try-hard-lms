8c05502e094c1e97d6a43229b3c0e82d6710f43c
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-02-25 15:43:50 +0700

    rfc: add MainLayout

--- src/pages/dashboard/student/index.tsx
+++ src/pages/dashboard/student/index.tsx
@@ -1,3 +1,13 @@
-export default function StudentPage() {
-  return <div>StudentPage</div>;
-}
+import MainLayout from "@/components/common/layouts/main/MainLayout";
+import StudentScreen from "@/components/student/StudentScreen";
+import { type NextPageWithLayout } from "@/types/page";
+
+const StudentPage: NextPageWithLayout = () => {
+  return <StudentScreen />;
+};
+
+export default StudentPage;
+
+StudentPage.getLayout = (page) => {
+  return <MainLayout>{page}</MainLayout>;
+};
