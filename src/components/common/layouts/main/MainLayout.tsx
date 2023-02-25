bd1995a01c4658deaa138ecfd471336f2f8b40c4
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-02-25 15:42:21 +0700

    feat: implement MainLayout

--- src/components/common/layouts/main/MainLayout.tsx
+++ src/components/common/layouts/main/MainLayout.tsx
@@ -1,12 +1,15 @@
+import { type FC, type ReactNode } from "react";
+import Navbar from "../../navbar/Navbar";
+
 interface IMainLayout {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-const MainLayout: React.FC<IMainLayout> = ({ children }) => {
+const MainLayout: FC<IMainLayout> = ({ children }) => {
   return (
-    <div className="relative min-h-screen">
-      <div className="absolute">Navbar</div>
-      <main className="pl-24">{children}</main>
+    <div className="relative min-h-screen bg-default-primary font-epilogue">
+      <Navbar />
+      <main className="py-8 pl-24">{children}</main>
     </div>
   );
 };
