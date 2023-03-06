a87ffe8f3ea76356664177795488054600441ea1
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-03-06 15:54:30 +0700

    feat: add main layout

--- src/components/common/layouts/main/MainLayout.tsx
+++ src/components/common/layouts/main/MainLayout.tsx
@@ -1,22 +1,31 @@
-import { useSession } from "next-auth/react";
 import { type FC, type ReactNode } from "react";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
 import LoadingScreen from "../../loading/LoadingScreen";
 import Navbar from "../../navbar/Navbar";
 
 interface IMainLayout {
+  title?: string;
   children: ReactNode;
 }
 
-const MainLayout: FC<IMainLayout> = ({ children }) => {
+const MainLayout: FC<IMainLayout> = ({ children, title = "Dashboard" }) => {
   const { status } = useSession();
 
   return (
-    <div className="relative min-h-screen bg-default-primary font-epilogue">
-      <Navbar />
-      <main className="pl-24 pr-6">
-        {status === "loading" ? <LoadingScreen /> : <>{children}</>}
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="bg-default-primary">
+        <div className="container relative mx-auto min-h-screen bg-default-primary font-epilogue">
+          <Navbar />
+          <main className="p-4 overflow-x-hidden lg:py-6 lg:pl-24 lg:pr-6">
+            {status === "loading" ? <LoadingScreen /> : <>{children}</>}
+          </main>
+        </div>
+      </div>
+    </>
   );
 };
 
