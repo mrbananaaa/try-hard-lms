deb67f233e2d6b3ab3d06fe55367a3e793d58c5e
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-03-06 15:54:44 +0700

    feat: implement navbar

--- src/components/common/navbar/base/NavbarBase.tsx
+++ src/components/common/navbar/base/NavbarBase.tsx
@@ -6,8 +6,8 @@ interface INavbarBase {
 
 const NavbarBase: FC<INavbarBase> = ({ children }) => {
   return (
-    <div className="fixed z-50 min-h-screen p-[1.5vh]">
-      <div className="flex min-h-[97vh] flex-col justify-between rounded-xl bg-black py-10">
+    <div className="fixed z-50 hidden min-h-screen p-[1.5vh] lg:block">
+      <div className="flex min-h-[97vh] flex-col justify-between rounded-xl bg-black py-10 2xl:min-h-[70vh]">
         <h1 className="px-4 font-semibold text-white">LMS</h1>
 
         <div className="flex flex-col items-center space-y-20">{children}</div>
