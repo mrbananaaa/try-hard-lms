86affc44dd94697780c2a3d08152535f6b8fc69b
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-03-06 15:58:58 +0700

    feat: implement student page

--- src/pages/dashboard/student/index.tsx
+++ src/pages/dashboard/student/index.tsx
@@ -1,13 +1,16 @@
 import MainLayout from "@/components/common/layouts/main/MainLayout";
 import StudentScreen from "@/components/student/StudentScreen";
 import { type NextPageWithLayout } from "@/types/page";
+import { useSession } from "next-auth/react";
 
 const StudentPage: NextPageWithLayout = () => {
-  return <StudentScreen />;
+  const { data: session } = useSession();
+
+  return <StudentScreen session={session} />;
 };
 
 export default StudentPage;
 
 StudentPage.getLayout = (page) => {
-  return <MainLayout>{page}</MainLayout>;
+  return <MainLayout title="Student Dashboard">{page}</MainLayout>;
 };
